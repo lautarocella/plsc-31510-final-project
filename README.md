07d4a74dcdf52369f7476c01440340be8ba09a1b
lautarocella <lcella@uchicago.edu>
2025-10-17 16:07:59 -0500

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# Short Description
+This project analyzes the ideological differences between center-left and center-right coalitions in Chile from 1990 to 2021 using State of the Union presidential speeches. First, I scraped the Chilean Congress website to collect all speeches. Second, I cleaned and pre-processed the text. Third, I identified distinctive words for each coalition. Finally, I used Wordfish, an unsupervised learning method, to estimate ideological positions of texts, and visualized the results.
+
+
---
+++ collect_data.R
@@ -0,0 +1,29 @@
+# Load packages
+library(tidyverse)
+library(rvest)
+library(lubridate)
+library(pdftools) 
+
+# Collecting data
+## I get the URLs of the speeches
+main <- read_html("https://www.bcn.cl/historiapolitica/corporaciones/cuentas_publicas/detalle?tipo=presidentes")
+
+url <- html_nodes(main, ".rspkr_dr_added") %>% 
+  html_attr("href")
+
+## Only keep speeches between 1990 and 2021
+url <- url[1:32]
+
+## I get the date of the speeches
+date <- html_nodes(main, ".rspkr_dr_added") %>% 
+  html_text()
+
+## I get the year of the speeches
+date <- dmy(date)
+year <- year(date)
+
+## Only keep years between 1990 and 2021
+year <- year[1:32]
+
+## Store text from all speeches 
+speeches <- lapply(url, pdftools::pdf_text)
